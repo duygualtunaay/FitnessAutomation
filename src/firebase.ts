0a6c535e19fe29b464214dc4991e8e71a85dd85a
duygualtunaay <147249839+duygualtunaay@users.noreply.github.com>
2025-06-17 11:36:18 +0300

    Fix Firebase duplicate app error

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -14,7 +14,7 @@ const firebaseConfig = {
     measurementId: "G-FH89WGMYXQ"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 
 export const auth = getAuth(app);
